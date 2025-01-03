603cc20af54bad2bbb80a24b9a683b09be105125
ImMikigo <mirkopedone03@gmail.com>
2025-01-04 00:37:12 +0100

    working on responsivity

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeLinkText() {
+
+    const cvBtnTxt = document.querySelector('#cv-btn-txt');
+
+    if (window.innerWidth <= 768) {
+        cvBtnTxt.textContent = 'CV';
+    } else {
+        cvBtnTxt.textContent = 'Curriculum Vitae';
+    }
+}
+
+// Esegui il controllo al caricamento della pagina
+changeLinkText();
+
+// Ascolta il resize della finestra
+window.addEventListener('resize', changeLinkText);
