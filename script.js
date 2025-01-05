c4887c5087f89d6a7edeef69330c499c12fba18b
ImMikigo <mirkopedone03@gmail.com>
2025-01-05 18:19:18 +0100

    started mobile variant (responsivity)

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
+
+// Update data Ultima Modifica
 function updateData() {
 
     const lastUpdateDate = document.getElementById('last-update');
 
-    lastUpdateDate.textContent = 'Ultimo aggiornamento: 04 Gen 2024';
+    lastUpdateDate.textContent = 'Ultimo aggiornamento: 05 Gen 2024';
     
 }
 
-// Esegui il controllo al caricamento della pagina
+// Imposta 'Mobile Variant' quando schermo max 768px 
+function enableMobileVariants() {
+
+    // Back to Home Button 
+    const btn_home        = document.getElementById('btn-home'       );
+    const btn_home_mobile = document.getElementById('btn-home-mobile'); 
+
+    // Certifications Section
+    const certifications        = document.getElementById('certifications'       );
+    const certifications_mobile = document.getElementById('certifications-mobile');
+
+    // Driving License Section
+    const drivingLicense        = document.getElementById('drivingLicense'       );
+    const drivingLicense_mobile = document.getElementById('drivingLicense-mobile');
+
+    if (window.innerWidth <= 768) {
+
+        // none - block
+
+        // Back to Home Button 
+        btn_home.style.display        = 'none';
+        btn_home_mobile.style.display = 'block'; 
+
+        // Certifications Section
+        certifications.style.display        = 'none';
+        certifications_mobile.style.display = 'block'; 
+
+        // Driving License Section
+        drivingLicense.style.display        = 'none';
+        drivingLicense_mobile.style.display = 'block'; 
+
+    } else {
+
+        // block - none
+
+        // Back to Home Button 
+        btn_home.style.display        = 'block';
+        btn_home_mobile.style.display = 'none'; 
+
+        // Certifications Section
+        certifications.style.display        = 'block';
+        certifications_mobile.style.display = 'none' ; 
+
+        // Driving License Section
+        drivingLicense.style.display        = 'block';
+        drivingLicense_mobile.style.display = 'none'; 
+
+    }
+}
+
+// Call Functions on Resize
+enableMobileVariants();
 updateData();
 
-// Ascolta il resize della finestra
+// Update al resize della finestra
 window.addEventListener('resize', updateData);
+window.addEventListener('resize', enableMobileVariants);
